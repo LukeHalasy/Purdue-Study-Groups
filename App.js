22246330c9194087c175319624434035a3e283c9
LiMinglun <li3019@purdue.edu>
2018-09-16 09:24:13 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,68 @@
+import * as React from 'react';
+import { Text, View, StyleSheet, NavigatorIOS } from 'react-native';
+import { Constants } from 'expo';
+import { navigationOptions } from 'react-navigation';
+import { createStackNavigator, createDrawerNavigator } from 'react-navigation';
+import { withNavigation } from 'react-navigation';
+
+// You can import from local files
+//import AssetExample from './components/AssetExample';
+
+import HomeScreen from './components/HomeScreen';
+import MyGroupScreen from './components/MyGroupScreen';
+import StudyProfile from './components/StudyProfile';
+import FindGroupScreen from './components/FindGroupScreen';
+import Login from './components/Login/Login';
+import Signup from './components/Signup/Signup';
+import AddStudyGroup from './components/AddStudyGroup/AddStudyGroup.js';
+
+// or any pure javascript modules available in npm
+import { Card } from 'react-native-elements'; // Version can be specified in package.json
+
+const Index = createStackNavigator(
+  {
+    Home: { screen: HomeScreen },
+    Login: { screen: Login },
+    Signup: { screen: Signup },
+    MakeGroup: { screen: AddStudyGroup },
+    MyGroups: { screen: MyGroupScreen },
+    StudyProfile: { screen: StudyProfile },
+    FindGroup: { screen: FindGroupScreen }
+  },
+  {
+    initialRouteName: 'Login'
+  }
+);
+
+export default class App extends React.Component {
+  render() {
+    return <Index />;
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    //alignItems: 'center',
+    //justifyContent: 'center',
+    //paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1'
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: '#34495e'
+  },
+  niceTitle: {
+    backgroundColor: 'skyblue',
+
+    textAlign: 'center',
+    fontSize: 50,
+    fontWeight: 'bold',
+
+    alignItems: 'center',
+    alignSelf: 'stretch'
+  }
+});
