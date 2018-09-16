b9a8b66bae145d6e1cb8c210b7a4c6a704746126
LukeHalasy <lhalasy@gmail.com>
2018-09-16 03:57:02 -0400

    Added database import file

---
+++ study-groups/database/database.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase';
+
+var config = {
+  apiKey: 'AIzaSyB2cxEKpYdG9HMSmk3dpf0FIfFnUYog8sg',
+  authDomain: 'purdueplanner-22f3f.firebaseapp.com',
+  databaseURL: 'https://purdueplanner-22f3f.firebaseio.com',
+  projectId: 'purdueplanner-22f3f',
+  storageBucket: 'purdueplanner-22f3f.appspot.com',
+  messagingSenderId: '358266043933'
+};
+
+firebase.initializeApp(config);
