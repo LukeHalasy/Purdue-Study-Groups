15edb0f2cf1359a53eb56441df8c1dd832099fe8
LiMinglun <li3019@purdue.edu>
2018-09-16 05:38:34 -0400

    Add files via upload

--- study-groups/components/StudyProfile.js
+++ study-groups/components/StudyProfile.js
@@ -1,14 +1,13 @@
 import * as React from 'react';
-import { Text, View, StyleSheet, NavigatorIOS
+import { Text, View, StyleSheet, NavigatorIOSm, ActivityIndicator
  } from 'react-native';
 import { Constants } from 'expo';
 import { List, ListItem } from 'react-native-elements'
 import { withNavigation } from 'react-navigation';
-
+import firebase from "firebase";
 // You can import from local files
 import AssetExample from './AssetExample';
 import Title from './Title';
-import Choose from './Choose';
 import Icon from './Icon';
 import CardHolder from './CardHolder';
 
@@ -17,25 +16,69 @@ import { Card } from 'react-native-elements'; // 0.19.1
 
 
 
-const list = [
+var avatarList = [
   {
-    name: 'Amy Farha',
-    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
-    subtitle: 'Vice President'
+    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg'
   },
   {
-    name: 'Chris Jackson',
-    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
-    subtitle: 'Vice Chairman'
-  },
+    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg'
+  },{
+    avatar_url: 'https://cdn.pixabay.com/photo/2015/02/28/09/32/beautiful-653317_1280.jpg'
+  },{
+    avatar_url: 'https://cdn.pixabay.com/photo/2016/11/08/16/03/manager-1808728_1280.jpg'
+  },{
+    avatar_url: 'https://cdn.pixabay.com/photo/2015/10/18/16/40/taxation-994707_1280.jpg'
+  },{
+    avatar_url: 'https://cdn.pixabay.com/photo/2016/07/27/08/00/female-model-1544783_1280.jpg'
+  },{
+    avatar_url: 'https://cdn.pixabay.com/photo/2016/07/27/07/57/male-1544775_1280.jpg'
+  }
 ]
 
 export default class StudyProfile extends React.Component {
+  constructor(props){
+      super(props);
+      this.state ={ isLoading: true, dataSource: []};
+  }
+  componentWillMount(){
+    var config = {
+      apiKey: "AIzaSyB2cxEKpYdG9HMSmk3dpf0FIfFnUYog8sg",
+      authDomain: "purdueplanner-22f3f.firebaseapp.com",
+      databaseURL: "https://purdueplanner-22f3f.firebaseio.com",
+      projectId: "purdueplanner-22f3f",
+      storageBucket: "purdueplanner-22f3f.appspot.com",
+      messagingSenderId: "358266043933"
+    };
+    if (!firebase.apps.length) {
+    firebase.initializeApp(config);}
+    database = firebase.database();
+    let retrnValue = firebase
+    .database()
+    .ref('Users/')
+    .once('value')
+    .then((snapshot) => {
+      let rawvalue = snapshot.val();
+      var resultValue = [];
+
+      for (var i in rawvalue){
+        rawvalue[i]["key"] = i;
+        resultValue.push(rawvalue[i]);
+      }
+
+      this.setState({
+        isLoading: false,
+        dataSource: resultValue,
+      });
+    });
+
+    return;
+  }
 
   render() {
     const test = this.props.navigation.getParam('users', 'RIP NOT RETURNED')[0];
     console.log(test);
-    return (
+    if(this.state.isLoading){
+      return(
         <View style={styles.container}>
           <Title title={this.props.navigation.getParam('title', 'Concept_Here')}/>
           <Text style={styles.description}>
@@ -43,34 +86,50 @@ export default class StudyProfile extends React.Component {
           <Text style={styles.description}>
           Location - {this.props.navigation.getParam('location', 'N/A')}</Text>
           <Text style={styles.description}>Members</Text>
-          <List containerStyle={styles.member}>
-          {
-            list.map((l) => (
-              <ListItem
-                roundAvatar
-                avatar={{uri:l.avatar_url}}
-                key={l.name}
-                title={l.name}
-              />
-            ))}
-          }
-         </List>
+          <ActivityIndicator/>
         </View>
-    );
+      )
+    }
+    else{
+      return (
+        <View style={styles.container}>
+          <Title title={this.props.navigation.getParam('title', 'Concept_Here')}/>
+          <Text style={styles.description}>
+          {this.props.navigation.getParam('startTime', 'Start_Time')} - {this.props.navigation.getParam('endTime', 'End_Time')}</Text>
+          <Text style={styles.description}>
+          Location - {this.props.navigation.getParam('location', 'N/A')}</Text>
+          <Text style={styles.description}>Members</Text>
+            <List containerStyle={styles.member}>
+            {
+              this.state.dataSource.map((l) => (
+                <ListItem
+                  roundAvatar
+
+                  avatar={{uri:avatarList[Math.floor(Math.random() * 7)].avatar_url}}
+                  key={l.name}
+                  title={l.name}
+                />
+              ))}
+            }
+           </List>
+          </View>
+      );
+    }
+
   }
 }
 
 const styles = StyleSheet.create({
   member: {
       marginBottom: 20,
-      flex:'stretch',
+      //flex:'stretch',
   },
   container: {
     alignItems: 'stretch',
     //justifyContent: 'center',
     //paddingTop: Constants.statusBarHeight,
     backgroundColor: '#ecf0f1',
-    flex:'stretch',
+    //flex:'stretch',
   },
   description: {
     margin: 24,
