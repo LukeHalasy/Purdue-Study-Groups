3c1b65bdc957d2c2d296e2cfe07ddaad39ae579b
chandiran-dmc <42678579+chandiran-dmc@users.noreply.github.com>
2018-09-16 03:11:50 -0400

    Footer File
    My first commit of the day lool. I did the footer. I also did the header which I will upload soon.

---
+++ study-groups/components/FooterComponent.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Button,View,StyleSheet,Text } from 'react-native';
+import { Container, Header, Footer, FooterTab, Icon } from 'native-base';
+import { createStackNavigator } from 'react-navigation';
+
+
+const styles = StyleSheet.create({
+  
+  footer: {
+    flexDirection:"row",
+    flex:1,    
+  },
+  footerButtonContainer:{
+    flex:1,
+    backgroundColor: '#162238'
+  }
+  
+
+})
+
+class FooterComponent extends React.Component {
+  render() {
+
+    return (
+        <Footer>
+          <FooterTab>
+          <View style={styles.footerButtonContainer}>
+            <Button active title='HELLO' color='#fff'>
+            </Button>
+          </View>  
+          <View style={styles.footerButtonContainer}>
+            <Button active title='HELLO' color='#fff'>
+              
+            </Button>
+            </View>
+            
+          </FooterTab>
+        </Footer>
+      )
+  }
+}
+
+export default FooterComponent;
+
+
+ 
