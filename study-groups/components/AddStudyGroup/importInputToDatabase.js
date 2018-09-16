735477dc1145f4eba063e7bef63f0c17c57dfb6c
Prakrit Duangsutha <prakrit_duangsutha@purdue.edu>
2018-09-16 01:58:54 -0400

    Added searching functions in importInputToDatabase.js

--- study-groups/components/AddStudyGroup/importInputToDatabase.js
+++ study-groups/components/AddStudyGroup/importInputToDatabase.js
@@ -29,8 +29,91 @@ export function importInputToDatabase(value) {
   return database.ref().update(updates);  
 }
 
+// todo: wire this function to a React component
 export function deleteInputInDatabase(id) {
   console.log(id);
   database.ref('studyGroups/' + id).remove();
 }
 
+// search for groups based on starting time
+// returns an array of objects
+export function searchTimeStart(timeStart) {
+  console.log('time start is ' + timeStart);
+
+  var result = [];
+
+  database.ref('studyGroups/')
+          .once('value')
+          .then(snapshot => {
+            var data = snapshot.val();
+
+            for (var k in data) {
+              if (data[k].timeStart == timeStart) {
+                result.push(data[k]);
+              } 
+            }
+          });
+
+  return result;
+}
+
+// search for groups based on ending time
+// returns an array of objects
+export function searchTimeEnd(timeEnd) {
+  console.log('time end is ' + timeEnd);
+
+  var result = [];
+
+  database.ref('studyGroups/')
+          .once('value')
+          .then(snapshot => {
+            var data = snapshot.val();
+
+            for (var k in data) {
+              if (data[k].timeEnd == timeEnd) {
+                result.push(data[k]);
+              } 
+            }
+          });
+
+  return result;
+}
+
+// search for groups based on course
+// returns an array of objects
+export function searchCourse(course) {
+  console.log('course is ' + course);
+
+  var result = [];
+
+  database.ref('studyGroups/')
+          .once('value')
+          .then(snapshot => {
+            var data = snapshot.val();
+
+            for (var k in data) {
+              if (data[k].course == course) {
+                result.push(data[k]);
+              } 
+            }
+          });
+
+  return result;
+}
+
+// automatically delete expired groups based on ending time
+export function deleteExpiredGroups() {
+  database.ref('studyGroups/')
+          .once('value')
+          .then(snapshot => {
+            var data = snapshot.val();
+            var currentTime = 10000000;
+
+            for (var k in data) {
+              // todo: compare time
+              if (data[k].timeEnd > currentTime) {
+                database.ref('studyGroup/' + data[k].id).remove();
+              }
+            }
+          });
+}
